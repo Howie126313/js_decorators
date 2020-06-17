af5089be08ff9aa2e2cc7aab0ada9b0c48dfacfd
Howie <howieforjob@163.com>
2020-06-17 10:47:25 +0800

    update

--- src/decorators/validators.js
+++ src/decorators/validators.js
@@ -2,7 +2,7 @@
 * @Author: Howie 
 * @Date: 2020-03-17 16:02:40 
  * @Last Modified by: Bryan
- * @Last Modified time: 2020-05-28 11:42:43
+ * @Last Modified time: 2020-06-17 10:22:14
 */
 import { property } from './decoratorsUntils'
 
@@ -47,13 +47,11 @@ export function validateArray() {
   return function (target, key, descriptor) {
     return property(target, key, descriptor, function (propertyName, val) {
       const realKey = propertyName.replace('_', '')
-      if (this[realKey] !== val) { // 生成正常的属性
-        if (!val instanceof Array) {
-          this[realKey] = val
-          console.error(`landz-sensors-fullstack 报错：神策${realKey}格式传入错误，应传入 Number 类型，当前${typeof(val)}`)
-        } else {
-          this[realKey] = val
-        }
+      if (!(val instanceof Array)) {
+        this[realKey] = val
+        console.error(`landz-sensors-fullstack 报错：神策${realKey}格式传入错误，应传入 Array 类型，当前${typeof(val)}`)
+      } else {
+        this[realKey] = val
       }
     })
   }
